63173c728061b4517b7f4feb05ff68c5ab59f78c
Miguel Felix <migfel@users.noreply.github.com>
2025-11-14 14:05:36 -0600

    Update FileServer.java

--- transferir fragmentos archivos, hilos/FileServer.java
+++ transferir fragmentos archivos, hilos/FileServer.java
@@ -2,12 +2,18 @@ import java.io.*;
 import java.net.*;
 
 public class FileServer {
+
     public static void main(String[] args) {
         try {
+            // Servidor escuchando en el puerto 12345
             ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("FileServer escuchando en el puerto 12345...");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
+
+                // Hilo para atender a cada cliente
                 Thread clientThread = new Thread(new ClientHandler(clientSocket));
                 clientThread.start();
             }
@@ -27,21 +33,23 @@ class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
-            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+            DataInputStream dis =
+                    new DataInputStream(clientSocket.getInputStream());
 
             // Leer nombre del archivo
             String fileName = dis.readUTF();
             System.out.println("Receiving file: " + fileName);
 
-            // Crear un nuevo archivo
+            // Crear un nuevo archivo (prefijo "received_")
             File file = new File("received_" + fileName);
             FileOutputStream fos = new FileOutputStream(file);
 
-            // Fragmentar y recibir el archivo
+            // Recibir el archivo en fragmentos
             byte[] buffer = new byte[1024];
             int bytesRead;
-            for (int i = 0; i < 10; i++) {
-                bytesRead = dis.read(buffer);
+
+            // Leer hasta que el cliente cierre el stream (read() = -1)
+            while ((bytesRead = dis.read(buffer)) != -1) {
                 fos.write(buffer, 0, bytesRead);
             }
 
