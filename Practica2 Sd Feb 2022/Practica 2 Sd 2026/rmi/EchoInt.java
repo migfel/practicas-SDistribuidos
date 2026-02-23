7d914e64d290a692e75a64b726c78817b6ee368a
Miguel Felix <migfel@users.noreply.github.com>
2026-02-23 15:27:44 -0600

    Delete Practica2 Sd Feb 2022 directory

--- Practica2 Sd Feb 2022/Practica 2 Sd 2026/rmi/EchoInt.java
+++
@@ -1,8 +0,0 @@
-
-package rmi;
-//Interfaz de tipo remota, cabe resaltar que
-// solo tiene el metodo echo ( no hya instrucciones) 
-public interface EchoInt extends java.rmi.Remote 
-{
-    public String echo(String input)throws java.rmi.RemoteException;
-}
