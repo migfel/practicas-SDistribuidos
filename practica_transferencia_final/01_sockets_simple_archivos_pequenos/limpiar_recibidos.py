e23f2edebf3a0882f2744a17ccecb53d2e26d31a
Miguel Felix <migfel@users.noreply.github.com>
2026-05-25 14:18:44 -0600

    Update limpiar_recibidos.py

--- practica_transferencia_final/01_sockets_simple_archivos_pequenos/limpiar_recibidos.py
+++ practica_transferencia_final/01_sockets_simple_archivos_pequenos/limpiar_recibidos.py
@@ -4,21 +4,9 @@ import shutil
 CARPETA = "recibidos"
 
 if os.path.exists(CARPETA):
+    shutil.rmtree(CARPETA)
+    print("Carpeta eliminada.")
 
-    archivos = os.listdir(CARPETA)
+os.makedirs(CARPETA, exist_ok=True)
 
-    total = len(archivos)
-
-    for archivo in archivos:
-        ruta = os.path.join(CARPETA, archivo)
-
-        try:
-            if os.path.isfile(ruta):
-                os.remove(ruta)
-        except Exception as e:
-            print(f"Error eliminando {archivo}: {e}")
-
-    print(f"Se eliminaron {total} archivos.")
-
-else:
-    print("La carpeta 'recibidos' no existe.")
+print("Carpeta limpia y recreada.")
